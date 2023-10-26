aa74443a2b162b37f1427220e8f3157a0e8a2036
mwasela <dennysmycall@gmail.com>
2023-10-26 06:49:38 +0300

    modified:   src/pages/Doors.jsx 	modified:   src/pages/Home.jsx

--- doorMon/src/pages/Home.jsx
+++ doorMon/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 
-import { CheckCard, StatisticCard } from '@ant-design/pro-components'
-import { Row, Col } from 'antd'
+import { CheckCard, StatisticCard, ProCard, ProTable } from '@ant-design/pro-components'
+import { Row, Col, Tag } from 'antd'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
@@ -9,14 +9,15 @@ import axios from 'axios'
 
 const { Statistic } = StatisticCard;
 
-axios.defaults.baseURL = 'http://localhost:3333/'
+axios.defaults.baseURL = 'http://10.168.3.233:3333/'
 
 export default function Home() {
   const [accessData, setAccessData] = useState([]);
   const [responsive, setResponsive] = useState(false);
   const [length, setLength] = useState(0);
   const [doorData, setdoorData] = useState([]);
- const [status, setStatus] = useState('offline');
+  const [status, setStatus] = useState('Offline');
+  const [deviceState, setDeviceState] = useState(0);
 
   //the use effect here is quite. Helpful.
 
@@ -28,36 +29,66 @@ export default function Home() {
 
   useEffect(() => {
     fetchDoors().then((data) => {
-        console.log("doors data", data);
-        setStatus('Online');
-     
+      console.log("doors data", data);
+      setStatus('Online');
+
     });
-}, [doorData]);
+  }, [doorData]);
 
 
-const _length = doorData.length;
-const _open = doorData.filter((door) => door.mgr_doors_state === "1").length;
-const _description = 
-`
+  const _length = doorData.length;
+  const _open = doorData.filter((door) => door.mgr_doors_state === "1").length;
+  const _description =
+    `
 Doors Connected -> ${_length} \n 
 Doors Opened ->${_open}  
 \n Status: ${status}`;
 
 
+  const checkDevice = async () => {
+    try {
+      const response = await axios.get('/doors');
+      console.log("response", response.data);
+      setDeviceState(reponse.data);
+    } catch (error) {
+      console(error);
+    }
+
+  }
+
+
   const fetchDoors = async () => {
     try {
-        const response = await axios.get('/doors');
-        console.log("response", response);
-        const length = response.data.length;
-        setdoorData(response.data);
-        setLength(length);
-        console.log("length", length);
-        return response.data;
+      const response = await axios.get('/doors');
+      console.log("response", response);
+      const length = response.data.length;
+      setdoorData(response.data);
+      setLength(length);
+      console.log("length", length);
+      feedAccesslogs(response.data);
+      return response.data;
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
   };
 
+
+  function feedAccesslogs(incomingData) {
+    incomingData.map((door) => {
+      if (door.mgr_doors_state === "1") {
+        console.log("door open");
+        axios.post('/logs', {
+          gbh_mgrmdraccesslogs_doors: door.id,
+          gbh_mgrmdraccesslogs_locations: door.mgr_doors_location,
+          gbh_mgrmdraccesslogs_terminal: door.mgr_doors_terminal,
+          mgr_accesslogs_state: door.mgr_doors_state,
+
+        })
+      } else {
+        return;
+      }
+    })
+  }
   const pingBackend = async () => {
     try {
       const response = await axios.get('/doors');
@@ -73,16 +104,11 @@ Doors Opened ->${_open}
       const response = await axios.get('/logs');
       setAccessData(response.data);
       //check length of array in response.dat
-  
       return response.data;
     } catch (error) {
       console.error(error);
     }
   };
-
-
-
-
 
   return (
     <>
@@ -175,36 +201,82 @@ Doors Opened ->${_open}
             defaultValue="A"
           >
 
-{
-  doorData.map((doorCard) => {
-    console.log("doorCard", doorCard )
+            {
+              doorData.map((doorCard) => {
+                console.log("doorCard", doorCard)
 
-    return (
-      <CheckCard
-      key={doorCard.id}
-              title={doorCard.mgr_doors_name}
-              description={doorCard.mgr_doors_state ==="1"? "Unlocked" : "Locked"}
-              value="A"
-              style={{
-                backgroundColor: doorCard.mgr_doors_state === "1" ? 'red' : 'green',
-                color: 'white',          // Set font color to white
-                fontSize: '16px',        // Increase font size by 3 pixels
-              }}
-            />
-  )
-  }
-  )
-}
-
-            
-
-            
-
-
+                return (
+                  <CheckCard
+                    key={doorCard.id}
+                    title={doorCard.mgr_doors_name}
+                    description={doorCard.mgr_doors_state === "1" ? "Unlocked" : "Locked"}
+                    value="A"
+                    style={{
+                      backgroundColor: doorCard.mgr_doors_state === "1" ? 'red' : 'green',
+                      color: 'white',          // Set font color to white
+                      fontSize: '16px',        // Increase font size by 3 pixels
+                    }}
+                  />
+                )
+              }
+              )
+            }
 
           </CheckCard.Group>
         </Col>
       </Row>
+
+      <ProTable
+          columns={[
+            {
+              title: 'TimeStamp',
+              dataIndex: 'mgr_accesslogs_time',
+              key: 'mgr_accesslogs_time',
+                valueType: 'dateTime',
+                render: (dom, entity) => {
+                  return dom
+                }
+            },
+            {
+              title: 'Door',
+              dataIndex: ['door', "mgr_doors_name"],
+              key: 'gbh_mgrmdraccesslogs_doors',
+            },
+            {
+              title: 'Location',
+              dataIndex: ["location", "mgr_location_name"],
+              key: 'mgr_doors_location',
+            },
+            {
+              title: 'Terminal',
+              dataIndex:  ["terminal", "mgr_terminal_name"],
+              key: 'mgr_doors_terminal',
+            },
+            {
+              title: 'State',
+              dataIndex: 'mgr_accesslogs_state',
+              key: 'mgr_accesslogs_state',
+              render: (dom, entity) => {
+                return dom === "1" ? <Tag color='error'>
+                  Unlocked
+                </Tag> : <Tag color='success'>
+                  Locked
+                </Tag>
+              }
+            },
+          ]}
+          dataSource={accessData}
+          rowKey="id"
+          search={false}
+          options={{
+            search: true,
+          }}
+          pagination={{
+            pageSize: 5,
+          }}
+          dateFormatter="string"
+          headerTitle="Door Events list"
+        />
 
     </>
   )
